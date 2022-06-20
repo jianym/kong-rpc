4a158ae88cacddfb4772cd76ab8219551ea9c662
jianym <3502168163@qq.com>
2022-06-20 12:42:36 +0800

    ssl传输未提交补充

---
+++ src/main/java/org/jeecf/kong/rpc/protocol/SslChannelInitializer.java
@@ -0,0 +1,15 @@
+//package org.jeecf.kong.rpc.protocol;
+//
+//import io.netty.channel.Channel;
+//import io.netty.channel.ChannelInitializer;
+//import io.netty.handler.ssl.SslContext;
+//
+//public abstract class SslChannelInitializer extends ChannelInitializer<Channel> {
+//
+//    private final SslContext context;
+//
+//    public SslChannelInitializer(SslContext context) {
+//        this.context = context;
+//    }
+//
+//}
